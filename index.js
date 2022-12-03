8b302deae820f7b8961b2ecf88ce4d5bf9932164
tokyoparis <115654236+tokyoparis@users.noreply.github.com>
2022-12-03 14:07:45 -0500

    final project

--- index.js
+++ index.js
@@ -30,21 +30,92 @@ randomButton.innerHTML = "Random"
 
 
 // client photo attribute
-clientImg.setAttribute("src", "/images/birkan.jpg");
-clientImg.setAttribute("width", "180");
+clientImg.setAttribute("width", "170");
 clientImg.setAttribute("height", "180");
-clientImg.style.borderRadius = '50%';
+clientImg.style.borderRadius = '30%';
 
 //element attribute
 container.setAttribute("class", "container");
+clientName.setAttribute("id", "clientName");
+clientJob.setAttribute("id", "clientJob");
+header.setAttribute("id", "header");
+clientImg.setAttribute("id", "clientImg");
+reviewBox.setAttribute("class", "reviewBox");
+randomButton.setAttribute("id", "randomButton");
+
+// random comments
+const obj = [
+    {
+        id: 1,
+        username: "Birkan",
+        userjob: "Developer",
+        review: "I can't express how much you are helping my learning journey. I was feeling so frustrated not being able to grasp JavaScript concepts.",
+        img: "/images/birkan.jpg"
+
+    },
+    {
+        id: 2,
+        username: "Nino ",
+        userjob: "Marketing",
+        review: "What you've done for us here is incredible. You've made me more confident to try writing some javascript on my own and helped me progress so much.",
+        img: "https://m.media-amazon.com/images/I/71CPNYUJQnL.jpg"
+    },
+    {
+        id: 3,
+        username: "Brian ",
+        userjob: "Sales man",
+        review: "Really versatile lessons here. Thanks for the resource",
+        img: "https://styles.redditmedia.com/t5_2tc6s/styles/communityIcon_vn92glo5ugy51.png?width=256&s=d6094f5c1ee7b4af8c030fce7278164b7a6ea560"
+    },
+    {
+        id: 4,
+        username: "Drake",
+        userjob: "Rapper",
+        review: "Exactly what I was looking for!",
+        img: "https://i.stack.imgur.com/AcfLu.jpg?s=256&g=1"
+    },
 
 
 
-//button event
+];
+
+
+
+// random reivew functions.
+
+let currentReview = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    reviewFunction();
+
+
+});
+
+
+
+function reviewFunction() {
+
+    const randomComments = obj[currentReview];
+    clientImg.src = randomComments.img;
+    clientName.innerHTML = randomComments.username;
+    clientJob.innerHTML = randomComments.userjob;
+    randomReview.innerHTML = randomComments.review;
+
+}
+
+
 randomButton.addEventListener("click", function () {
 
 
-    randomReview.textContent = "You just clicked random button!"
+    currentReview = Math.floor(Math.random() * obj.length);
 
+    reviewFunction();
 
 })
+
+
+
+
+
+
